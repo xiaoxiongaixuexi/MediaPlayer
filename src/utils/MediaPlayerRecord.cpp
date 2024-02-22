059f4c99718c7bb940825376dd3e11690bd08224
小熊熊熊 <981303787@qq.com>
2024-02-22 22:26:56 +0800

    搭建播放记录实现框架

---
+++ src/utils/MediaPlayerRecord.cpp
@@ -0,0 +1,124 @@
+﻿#include "MediaPlayerRecord.h"
+#include "os_log.h"
+
+#include <io.h>
+#include <windows.h>
+#include <algorithm>
+
+CMediaPlayerRecord::CMediaPlayerRecord(const std::string & path) :
+    _record_path(path)
+{
+    if (!loadRecord())
+    {
+        log_msg_warn("Load record file %s failed!", path.c_str());
+    }
+}
+
+CMediaPlayerRecord::~CMediaPlayerRecord()
+{
+    if (!saveRecordToFile())
+    {
+        log_msg_warn("Save record to file %s failed!", _record_path.c_str());
+    }
+    unloadRecord();
+    _record_path.clear();
+}
+
+bool CMediaPlayerRecord::addRecord(const std::string & record)
+{
+    if (record.empty())
+    {
+        log_msg_warn("Input record is empty in file %s.", _record_path.c_str());
+        return false;
+    }
+
+    if (_lst.end() == std::find(_lst.begin(), _lst.end(), record))
+    {
+        _lst.emplace_back(record);
+    }
+
+    return true;
+}
+
+bool CMediaPlayerRecord::delRecord(const std::string & record)
+{
+    if (record.empty())
+    {
+        log_msg_warn("Input record is empty in file %s.", _record_path.c_str());
+        return false;
+    }
+
+    auto iter = std::find(_lst.begin(), _lst.end(), record);
+    if (_lst.end() != iter)
+    {
+        _lst.erase(std::remove(_lst.begin(), _lst.end(), record), _lst.end());
+    }
+
+    return true;
+}
+
+bool CMediaPlayerRecord::modRecord(const std::string & src, const std::string & dst)
+{
+    if (src.empty() || dst.empty())
+    {
+        log_msg_warn("Input record is empty in file %s.", _record_path.c_str());
+        return false;
+    }
+
+    auto iter = std::find(_lst.begin(), _lst.end(), src);
+    if (_lst.end() != iter)
+    {
+        *iter = dst;
+    }
+
+    return true;
+}
+
+bool CMediaPlayerRecord::getRecord(std::list<std::string> & records)
+{
+    records.clear();
+
+    for (const auto & record : _lst)
+    {
+        records.emplace_back(record);
+    }
+
+    return true;
+}
+
+bool CMediaPlayerRecord::loadRecord()
+{
+    if (_record_path.empty())
+    {
+        log_msg_warn("Record file is empty!");
+        return false;
+    }
+
+    if (!_access(_record_path.c_str(), 0))
+    {
+        return true;
+    }
+
+    //_cnt = GetPrivateProfileInt("TotalRecords", "cnt", 0, _record_path.c_str());
+    //for (int i = 0; i < _cnt; ++i)
+    //{
+    //    char buff[16] = { 0 };
+    //    char path[MAX_PATH] = { 0 };
+    //    snprintf(buff, 16, "Record%d", i + 1);
+    //    GetPrivateProfileString(buff, "path", "", path, MAX_PATH, _record_path.c_str());
+    //    _lst.emplace_back(path);
+    //}
+
+    return true;
+}
+
+void CMediaPlayerRecord::unloadRecord()
+{
+    _lst.clear();
+    _cnt = 0;
+}
+
+bool CMediaPlayerRecord::saveRecordToFile()
+{
+    return true;
+}
---
+++ src/utils/MediaPlayerRecord.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <cstdio>
+#include <cstdbool>
+#include <string>
+#include <list>
+
+class CMediaPlayerRecord final
+{
+public:
+    CMediaPlayerRecord(const std::string& path);
+    ~CMediaPlayerRecord();
+
+    // 添加记录
+    bool addRecord(const std::string& record);
+    // 删除记录
+    bool delRecord(const std::string& record);
+    // 修改记录
+    bool modRecord(const std::string& src, const std::string& dst);
+    // 获取记录
+    bool getRecord(std::list<std::string>& records);
+
+private:
+    // 加载配置文件
+    bool loadRecord();
+    // 释放
+    void unloadRecord();
+    // 保存到文件中
+    bool saveRecordToFile();
+
+private:
+    // 文件路径
+    std::string _record_path;
+
+    // 记录个数
+    int _cnt = 0;
+    // 记录列表
+    std::list<std::string> _lst;
+};
